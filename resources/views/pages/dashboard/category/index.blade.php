1d51bd36b8ae64268a69be3f6d85c56a33b9101b
DDay <nur.andromeda@gmail.com>
2022-02-14 02:09:51 +0700

    update from devstore

--- resources/views/pages/dashboard/category/index.blade.php
+++ resources/views/pages/dashboard/category/index.blade.php
@@ -30,7 +30,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mb-10">
-                <a href="{{ route('dashboard.category.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
+                <a href="{{ route('dashboard.category.create') }}"
+                 class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
                     + Create Category
                 </a>
             </div>
