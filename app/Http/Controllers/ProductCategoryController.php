00d9147fa134d8b99c0ceb008a899d92f1f502c4
DDay <nur.andromeda@gmail.com>
2022-02-14 11:30:11 +0700

    bug fix index pgsql

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -52,7 +52,7 @@ class ProductCategoryController extends Controller
      */
     public function create()
     {
-        DB::statement("ALTER SEQUENCE product_categories_id_seq RESTART WITH 0;");
+        DB::statement("ALTER SEQUENCE product_categories_id_seq RESTART WITH 1;");
         return view('pages.dashboard.category.create');
     }
 
