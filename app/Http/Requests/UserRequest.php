00b9d7987d5fc3016f80dc2d6b7e8461ef490ecb
DDay <nur.andromeda@gmail.com>
2022-07-28 19:12:31 +0700

    userrequest role

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,7 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255',
             'username' => 'required|string|max:255',
-            'roles' => 'required|string|max:255|in:USER,ADMIN'
+            'role' => 'required|string|max:255|in:USER,ADMIN'
         ];
     }
 }
