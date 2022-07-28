68f808c515e1bac9b9bcedbc34c0aee780ce4af5
DDay <nur.andromeda@gmail.com>
2022-07-28 19:08:28 +0700

    transaksi dan user control dan request

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\FileUploadController;
 use App\Http\Controllers\ProductGalleryController;
-use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ProductCategoryController;
 
 /*
