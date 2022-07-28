52fdd694d0e413163c505162181cea5e07cdbc7b
DDay <nur.andromeda@gmail.com>
2022-07-28 18:05:58 +0700

    show by name category

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -17,9 +17,9 @@ class ProductController extends Controller
         $name = $request->input('name');
         $description = $request->input('description');
         $tag = $request->input('tag');
-        $categories = $request->input('categories');
-        $price_from = $request->input('price_from');
-        $price_to = $request->input('price_to');
+        $categories = $request->input('categories_id');
+        $price = $request->input('price');
+        // $price_to = $request->input('price');
 
         if ($id) {
             $product = Product::with(['category', 'galleries'])->find($id);
@@ -41,22 +41,22 @@ class ProductController extends Controller
         $product = Product::with(['category', 'galleries']);
 
         if ($name) {
-            $product->where('name', 'like', '%', $name, '%');
+            $product->where('name', 'like', "%{$name}%");
         }
         if ($description) {
-            $product->where('description', 'like', '%', $description, '%');
+            $product->where('description', 'like', "%{$description}%");
         }
         if ($tag) {
-            $product->where('tag', 'like', '%', $tag, '%');
+            $product->where('tag', 'like', "%{$tag}%");
         }
         if ($categories) {
-            $product->where('categories', $categories);
+            $product->where('categories_id', 'like', "%$categories%");
         }
-        if ($price_from) {
-            $product->where('price_from', '>=', $price_from);
+        if ($price) {
+            $product->sortBy('price');
         }
-        if ($price_to) {
-            $product->where('price_to', '<=', $price_to);
+        if ($price) {
+            $product->orderBy('price', 'asc');
         }
         return ResponseFormatter::success(
             $product->paginate($limit),
